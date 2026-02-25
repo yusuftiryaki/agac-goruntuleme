1676974f6c3717bf912ffe904ae48453e8e6e012
Yusuf Tiryaki <yusuf.tiryaki@gmail.com>
2026-02-25 22:47:34 +0000

    actions.ts:9  POST https://6000-firebase-studio-1772049325519.cluster-y

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '100mb',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
