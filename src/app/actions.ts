efdf3de59bc8ef167cf43646be7ccafb40bb005d
Yusuf Tiryaki <yusuf.tiryaki@gmail.com>
2026-02-26 15:16:51 +0000

    https://studio--studio-166104040-52130.us-central1.hosted.app/icons/icon

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -44,7 +44,7 @@ export async function handleVideoUpload(formData: FormData) {
     console.log("Adding document to Firestore 'trees' collection.");
     await addDoc(collection(db, 'trees'), {
       video_url,
-      status: 'Bekliyor',
+      status: 'pending',
       timestamp: serverTimestamp(),
     });
     console.log("Successfully added document to Firestore.");
