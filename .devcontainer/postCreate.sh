155a7c8e4dabf4a0310411d8bb779077cffdb813
Yusuf Tiryaki <yusuf.tiryaki@gmail.com>
2026-03-02 22:03:52 +0000

    feat: firebase deploy

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Geliştirme ortamı başlatıldığında çalışacak komutlar buraya eklenebilir
 # Örneğin, veritabanı başlatma veya özel scriptler
-
+sudo npm install -g firebase-tools
 echo "Devcontainer başlatıldı. Node ve npm kurulu."
